785fad317d131f9105bed06ebc499b1a71c3aa14
aram426 <amy0426@cau.ac.kr>
2026-03-19 20:10:19 +0900

    FEAT: "0주차 미션1 투두리스트 완성, 저장 기능 미구현"

---
+++ Marimo/week0/toDoListJs.js
@@ -0,0 +1,55 @@
+const input = document.getElementById("input");
+const todoList = document.getElementById("todo-list");
+const doneList = document.getElementById("done-list");
+
+
+
+input.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        const task = input.value.trim();
+        if (task) {
+            const li = document.createElement("li");
+            const span = document.createElement("span");
+            span.textContent = task;
+            const completeButton = document.createElement("button");
+            completeButton.textContent = "완료";
+            li.append(span, completeButton);
+            todoList.appendChild(li);
+            input.value = "";
+        }
+    }
+    
+});    
+
+todoList.addEventListener("click", function(event) {
+    if (event.target.tagName === "BUTTON" && event.target.textContent === "완료") {
+        const li = event.target.parentElement;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "삭제";
+        li.append(deleteButton);
+        doneList.appendChild(li);
+        event.target.remove();
+    }
+});
+
+
+doneList.addEventListener("click", function(event){
+    if (event.target.tagName === "BUTTON" && event.target.textContent === "삭제") {
+        const li = event.target.parentElement;
+        li.remove();
+    }
+})
+
+const savedTodos = localStorage.getItem("todos");
+if (savedTodos !== null){
+    const parsedTodos = JSON.parse(savedTodos);
+    parsedTodos.forEach(function(todo){
+        const li = document.createElement("li");
+        const span = document.createElement("span");
+        span.textContent = todo.task;
+        const completeButton = document.createElement("button");
+        completeButton.textContent = "완료";
+        li.append(span, completeButton);
+        todoList.appendChild(li);
+    });
+}
